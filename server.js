ef339c6927d920bfb333ae66fb3262dadbb42ed6
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2023-03-29 18:15:35 -0700

    Nested folder temp fix

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const bodyParser = require('body-parser')
+require('dotenv').config()
 
 
 const app = express()
 
 //middleware
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(cors())
 
 //connect to DB
@@ -23,7 +23,35 @@ mongoose.connect(process.env.MONGODB_URI, {
 //Mongoose schema for form data
 const formDataSchema = new mongoose.Schema({
     name: String,
-    cats: String,
-    dogs: String
+    email: String,
+    message: String,
+    // dayOfWeek: assignDayOfWeek(value.date),
+    // timeOfDay: getTimeOfDay(value.date),
+    // groupByDay: groupByDay(),
+    // groupByWeek: groupByWeek()
+
 })
 
+//Mongoose model for the form data
+const FormData = mongoose.model('FormData', formDataSchema)
+
+//route to handle form submissions
+app.post('/form', async (req, res) => {
+    try {
+        //create a new FormData object from the request body
+        const formData = new FormData(req.body)
+        //Save form data to db
+        await formData.save()
+        res.status(200).send('Form submitted successfully')
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Error submitting form')
+    }
+})
+
+
+
+//Start server
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server started on port ${port}`))
