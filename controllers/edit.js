9fe035bd8eaf5b3f8ba36fd1b950ce1b82a2c0d8
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2023-07-17 08:52:32 -0700

    fixed medication taken bug

--- controllers/edit.js
+++ controllers/edit.js
@@ -16,6 +16,7 @@ module.exports = {
         TicketList.findByIdAndUpdate(
             id,
             {
+                status: 'Closed',
                 status: 'Medication Taken'
             },
         )    
