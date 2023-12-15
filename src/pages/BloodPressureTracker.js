b673382fbd2208870dc2bc96c304adc7932434c6
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2023-12-15 07:12:03 -0800

    save.

--- src/pages/BloodPressureTracker.js
+++ src/pages/BloodPressureTracker.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const BloodPressureTracker = ({data}) => {
+const BloodPressureTracker = ({data}) => { //change data
     const [selectedWeekIndex, setSelectedWeekIndex] = useState(0)
     const selectedWeek = data[selectedWeekIndex] || []
 
@@ -20,16 +20,16 @@ const BloodPressureTracker = ({data}) => {
             {selectedWeek.map((day, index) => (
                 <div key={index}>
                     <h3>{day[0]?.dayOfWeek || 'No Data'}</h3>
-                    <Card.Body>
+                    {/* <Card.Body>
                         <Card.Title>Time of Day: {post.timeOfDay}</Card.Title>
                         <Card.Text>Blood Pressure: {post.bloodPressure}</Card.Text>
                         <Card.Text>Severity: {post.severity}</Card.Text>
                         <Card.Text>Assigned To: {post.assignedTo}</Card.Text>
                         <Card.Text>Status: {post.status}</Card.Text>
                         <Card.Text>Date: {post.date}</Card.Text>
-                        <Button variant="danger" onClick={() => onDelete(post._id)}>Delete</Button>
+                        <Button variant="danger" onClick={() => onDelete(post._id)}>Delete</Button> */}
                         {/* <Button variant="warning" onClick={() => onLogout(user._id) }>Logout</Button> */}
-                    </Card.Body>
+                    {/* </Card.Body> */}
 
                 </div>    
             ))}
